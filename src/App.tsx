f0811211f1cdf96d05e1c4d04887755d1ef26107
rd273001 <rd273001@gmail.com>
2024-12-01 23:17:01 +0530

    Initial ToDo app - Basic setup including libraries, structure, configs

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+
+  return (
+    <h1 className='text-3xl font-bold text-purple-400'>
+      Initial ToDo App Created with basic setup
+    </h1>
+  )
+};
+
+export default App;
